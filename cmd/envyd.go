280f041f59b7ec8ac369c8da4868016e87b41bcf
Fabio Rehm <fgrehm@gmail.com>
2015-06-25 20:19:28 -0300

    :boom: Hack in support for building github projects using a Devstep base image :boom:

--- cmd/envyd.go
+++ cmd/envyd.go
@@ -55,5 +55,40 @@ func main() {
 			return pty
 		})
 	})
+	http.HandleFunc("/gh/", func(w http.ResponseWriter, r *http.Request) {
+		parts := strings.Split(r.URL.Path, "/")
+		if len(parts) < 5 {
+			http.NotFound(w, r)
+			return
+		}
+		pathUser := parts[2]
+		var pathEnv, sshUser string
+		if len(parts) == 6 {
+			pathEnv = parts[3] + "/" + parts[4]
+			sshUser = pathUser + "+" + pathEnv
+		} else {
+			sshUser = pathUser
+		}
+		// passthrough auth for hterm. use cookie to do this right
+		if !strings.Contains(r.URL.Path, "hterm") {
+			user, passwd, ok := r.BasicAuth()
+			if !ok || user != pathUser || !githubAuth(user, passwd) {
+				w.Header().Set("WWW-Authenticate", fmt.Sprintf("Basic realm=\"%s\"", pathUser))
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+		}
+		w.Header().Set("Hterm-Title", "Envy Term")
+		hterm.Handle(w, r, func(args string) *hterm.Pty {
+			cmd := exec.Command("/bin/enterenv", parts[2])
+			cmd.Env = os.Environ()
+			cmd.Env = append(cmd.Env, fmt.Sprintf("USER=%s", sshUser))
+			pty, err := hterm.NewPty(cmd)
+			if err != nil {
+				log.Fatal(err)
+			}
+			return pty
+		})
+	})
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
